7a3da017d8f85be29fcc3d166a4accf903ffd6d5
Adam N <noonan.adamw@gmail.com>
2025-08-28 01:06:24 +0100

    idk

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+Signed Distance Field visualiser using C++ and Raylib
+
+CONTROLS:
+
+W+S:		sine displacement
+Q+E:		sine frequency
+1+2:		smoothing strength
+3+4:		box corner rounding
+SPACEBAR: 	toggle shape outlines
+
+Click and hold to drag shapes
--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 // test.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+// includes
 #include <iostream>
 #include "raylib.h"
-#include "raylib-cpp.h"
 #include "raymath.h"
 
+// namespace
 using namespace std;
 
 // funcs
@@ -16,7 +17,9 @@ int twoDtoOneD(int, int, int);
 Vector2 oneDtoTwoD(int, int);
 float smoothMin(float, float, float);
 Vector2 absVec(Vector2);
+void drawPixelScaled(int, int, Color, int, int);
 
+// main method
 int main()
 {
     // vars
@@ -26,6 +29,7 @@ int main()
 
     const int width{ 400 };
     const int height{ 300 };
+    const int resScale = 2; // divide resolution by this amount
 
     Vector2 circle1 = { width/2, height/2 };
     float radius1 = 50;
@@ -34,10 +38,13 @@ int main()
     Vector2 rect = { 300, 200 };
     Vector2 rectDims = { 80, 60 };
     float rectRound = 50;
+    float maxRound = min(rectDims.x, rectDims.y);
     bool select2 = false;
 
     float moveSpeed = 60;
     float smoothing = 15;
+
+    bool isDebug = false;
 
     Vector2 mouseOffset = { 0, 0 };
 
@@ -46,67 +53,10 @@ int main()
     InitWindow(width, height, "program");
     SetTargetFPS(60);
 
-    // pixel stuff
-    Color* singlePixelColArray = new Color[width * height];
 
-    for (int i = 0; i < width*height; i++)
-    {
-        Vector2 pt = oneDtoTwoD(i, width);
-        float sinValue = amplitude * sin(sdCircle(pt, circle1, radius1) * frequency) + displace;
-        singlePixelColArray[i].g = sinValue;
-    }
-
-    Image img;
-    img.data = singlePixelColArray;
-    img.width = width;
-    img.height = height;
-    img.mipmaps = 1;
-    img.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
-
-    Texture2D tex = LoadTextureFromImage(img);
-
-    Rectangle src{ 0, 0, width, height};
-    Rectangle dest{ 0, 0, width, height};
-    Vector2 org{ 0, 0 };
-    float rot{ 0 };
-        
+    // main loop   
     while (WindowShouldClose() == false)
     {
-        for (int i = 0; i < width * height; i++)
-        {
-            Vector2 pt = oneDtoTwoD(i, width);
-            float dst = smoothMin(sdCircle(pt, circle1, radius1), sdBox(pt, rect, rectDims, rectRound), smoothing);
-            float sinValue = amplitude * sin(dst * frequency) + displace;
-
-            int colValue = 50;
-
-            if (sinValue > 0)
-            {
-                colValue = 200;
-            }
-
-
-            if (abs(dst) < 3) // border
-            {
-                singlePixelColArray[i].r = 0;
-                singlePixelColArray[i].g = 0;
-                singlePixelColArray[i].b = 0;
-            }
-            else if (dst < 0) // inside
-            {
-                singlePixelColArray[i].r = 200;
-                singlePixelColArray[i].g = colValue;
-                singlePixelColArray[i].b = 0;
-            }
-            else // outside
-            {
-                singlePixelColArray[i].r = 250;
-                singlePixelColArray[i].g = 100;
-                singlePixelColArray[i].b = colValue;
-            }
-        }
-
-        UpdateTexture(tex, singlePixelColArray);
 
         // input
         if (IsKeyDown(KEY_A))
@@ -142,12 +92,12 @@ int main()
         if (IsKeyDown(KEY_V))
         {
             printf("\nSDF AT MOUSE: ");
-            cout << sdCircle(GetMousePosition(), circle1, radius1);
+            //cout << sdCircle(GetMousePosition(), circle1, radius1);
+            cout << GetMousePosition().x << " " << GetMousePosition().y;
         }
 
         // CIRCLE MOVEMENT
         
-
         // select circle
         if (IsMouseButtonPressed(0))
         {
@@ -206,13 +156,83 @@ int main()
         {
             rectRound = 0.0f;
         }
+        else if (rectRound >= maxRound)
+        {
+            rectRound = maxRound;
+        }
+
+        // toggle debug
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            isDebug = !isDebug;
+        }
         
 
         BeginDrawing();
-        DrawTexturePro(tex, src, dest, org, rot, WHITE);
+
+        Color c = { 0, 0, 0 , 255 };
+        float dst;
+        float sinValue;
+        unsigned char colValue = 50;
+
+        Color prevXCol;
+        Color prevYCol;
+
+        // loop over every pixel to get colour
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+
+                Vector2 pt = { x, y };
+
+                dst = smoothMin(sdCircle(pt, circle1, radius1), sdBox(pt, rect, rectDims, rectRound), smoothing); // circle and rect
+                //dst = sdCircle(pt, circle1, radius1); // circle
+                //dst = sdBox(pt, rect, rectDims, rectRound); // rect
+
+                // get sin from dst calculation
+                sinValue = amplitude * sin(dst * frequency) + displace;
+           
+                // switch between 2 values based on result
+                colValue = 50;
+                if (sinValue > 0)
+                {
+                    colValue = 200;
+                }
+                
+
+                if (abs(dst) < 3) // border
+                {
+                    c = { 0, 0, 0 , 255 };
+                }
+                else if (dst < 0) // inside
+                {
+                    c = { 200, colValue, 50, 255 };
+                }
+                else // outside
+                {
+
+                    c = { 250, 100, colValue, 255 };
+                }
+
+                // draw four pixels for each calculation (50% resolution scale)
+                drawPixelScaled(x, y, c, resScale, resScale);
+                y += resScale - 1;
+            }
+            x += resScale - 1;
+        }
+
+        // shape outlines ( unaffected by res scale )
+        if (isDebug)
+        {
+            DrawCircleLines(circle1.x, circle1.y, radius1, BLACK);
+            Vector2 alteredRect = rect - rectDims;
+            Vector2 alteredDims = rectDims * 2;
+            Rectangle r = { alteredRect.x, alteredRect.y, alteredDims.x, alteredDims.y };
+            DrawRectangleRoundedLines(r, rectRound / maxRound, 2, BLACK);
+        }
 
         DrawFPS(10, 10);
-        ClearBackground(WHITE);
 
         EndDrawing();
     }
@@ -221,12 +241,15 @@ int main()
     return 0;
 }
 
-// distance fields
+//----------SIGNED DISTANCE FIELDS
+// circle distance field
 float sdCircle(Vector2 point, Vector2 center, float radius)
 {
     Vector2 pointTrans = point - center;
     return vecLen(pointTrans) - radius;
 }
+
+// box distance field
 float sdBox(Vector2 point, Vector2 center, Vector2 b, float rounding = 0.0f)
 {
     b.x -= rounding;
@@ -236,16 +259,19 @@ float sdBox(Vector2 point, Vector2 center, Vector2 b, float rounding = 0.0f)
     return Vector2Length(Vector2Max(d, Vector2Zero())) + min(max(d.x, d.y), 0.0f) - rounding;
 }
 
+// length of vector
 float vecLen(Vector2 v)
 {
     return sqrt((v.x * v.x) + (v.y * v.y));
 }
 
+// convert 2D matrix entry to array index
 int twoDtoOneD(int row, int rowSize, int col)
 {
     return (row * rowSize) + col;
 }
 
+// convert array index to 2D matrix entry
 Vector2 oneDtoTwoD(int index, int rowSize)
 {
     int rowsPassed = 0;
@@ -259,24 +285,31 @@ Vector2 oneDtoTwoD(int index, int rowSize)
 
 }
 
+// absolute vector value
 Vector2 absVec(Vector2 v)
 {
     return { abs(v.x), abs(v.y) };
 }
 
+// smooth minimum of 2 floats
 float smoothMin(float a, float b, float k)
 {
+    if (k == 0)
+    {
+        return min(a, b);
+    }
     float h = max(k - abs(a - b), 0.0f) / k;
     return min(a, b) - h * h * k * (1.0 / 4.0);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// draw a big pixel
+void drawPixelScaled(int x, int y, Color c, int xSpan, int ySpan)
+{
+    for (int i = 0; i < xSpan; i++)
+    {
+        for (int j = 0; j < ySpan; j++)
+        {
+            DrawPixel(x + i, y + j, c);
+        }
+    }
+}
